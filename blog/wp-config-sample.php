f2bc8412b81984dbcfbad4eb47f227e0ee901bbf
Joshua Vegas <joshuavegas@MC-G-LP-C120-15.local>
2017-03-08 18:22:39 +0100

    Updated worpdress and fixed serach display

--- blog/wp-config-sample.php
+++ blog/wp-config-sample.php
@@ -19,13 +19,13 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define('DB_NAME', 'extrablog');
+define('DB_NAME', 'votre_nom_de_bdd');
 
 /** Utilisateur de la base de données MySQL. */
-define('DB_USER', 'root');
+define('DB_USER', 'votre_utilisateur_de_bdd');
 
 /** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'votre_mdp_de_bdd');
 
 /** Adresse de l’hébergement MySQL. */
 define('DB_HOST', 'localhost');
